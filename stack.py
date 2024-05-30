4a215b4868a23334665daa040c44ae2b6c13480d
Dhanush <eaglephatidhanush@gmail.com>
2024-05-30 08:34:50 +0530

    stack structure defined

---
+++ stack.py
@@ -0,0 +1,22 @@
+import numpy as np 
+
+
+class Stack:
+
+    def __init__(self):pass
+
+    def push(self):pass
+
+    def pop(self):pass
+
+    def peek(self):pass
+
+    def size(self):pass
+
+    def isEmpty(self):pass
+
+    def isFull(self):pass
+
+    def clear(self):pass
+
+    def tolist(self):pass
